5de85a3b4c603f129919bc0e3e397b99b10e8adc
Suman Sourabh <sumsourabh14@gmail.com>
2022-09-14 14:26:31 +0530

    edit navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,24 @@
 import './navbar.css';
+import logo from '../../assets/logo.svg';
 
 function Navbar() {
   return (
     <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
-        
+        <div className='gpt3__navbar-links_logo'>
+          <img src={logo} alt="logo" />
+        </div>
+        <div className='gpt3__navbar-links_container'>
+          <p><a href='#home'>Home</a></p>
+          <p><a href='#whatgpt3'>What is GPT3</a></p>
+          <p><a href='#possibiligy'>Open AI</a></p>
+          <p><a href='#features'>Case Studies</a></p>
+          <p><a href='#blog'>Libraries</a></p>
+        </div>
+        <div className='gpt3__navbar-sign'>
+          <p>Sign in</p>
+          <button type='button'>Sign up</button>
+        </div>
       </div>
     </div>
   )
