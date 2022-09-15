af1a587cb715d50a7f3908679df83a1045ee02f2
Suman Sourabh <sumsourabh14@gmail.com>
2022-09-16 00:52:09 +0530

    update navbar menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,10 @@ import close from '../assets/close.svg';
 function Navbar() {
     const [toggle, setToggle] = useState(false);
 
+    function handleToggle() {
+        setToggle(!toggle)
+    }
+
   return (
     <nav className='font-outfit py-8 text-white flex justify-between items-center'>
         <p className='uppercase font-semibold'>Krypto</p>
@@ -23,6 +27,30 @@ function Navbar() {
                 )
             })}
         </ul>
+        <div className='sm:hidden'>
+            <img 
+                src={toggle ? close : menu} 
+                onClick={handleToggle} 
+                alt="menu" 
+                className='w-[30px] h-[30px] object-contain cursor-pointer'
+            />
+            <div className={`${toggle ? "flex" : "hidden"} p-6 absolute top-20 right-0 left-0 bg-blue-900 mx-0 my-2 bg-opacity-95 `}>
+                <ul className="list-none flex justify-end items-start flex-1 flex-col">
+                    {navLinks.map((nav, index) => {
+                        return (
+                            <li key={nav.id}>
+                                <a
+                                    href={`#${nav.id}`}
+                                    className={index === navLinks.length - 1 ? "mb-0" : "mb-2"}
+                                >
+                                    {nav.nav}
+                                </a>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        </div>
     </nav>
   )
 }
